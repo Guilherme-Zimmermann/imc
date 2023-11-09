ebe8754eb5753aea01168a166ffa8db8ebfc475d
GuiZimmermann <zimmerma83@gmail.com>
2023-11-09 08:49:03 -0400

    fix: Corrigindo alguns bugs

--- Application.java
+++ Application.java
@@ -1,5 +1,6 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import exceptions.ErroNumeroNegativo;
 
 public class Application {
     
@@ -13,25 +14,32 @@ public class Application {
                 System.out.print("Por favor, insira o seu peso(kg): ");
                 String s1 = sc.nextLine();
 
+                if (s1.contains("-")) {
+                    throw new ErroNumeroNegativo("Por favor, insira apenas valores positivos!");
+                }
+
                 String[] aux1 = s1.split(" ");
-                Double peso = Double.parseDouble(aux1[0]);
+                Double peso = Double.parseDouble(aux1[0].replace(",", "."));
                 
                 System.out.print("Por favor, insira a sua altura(m): ");
                 String s2 = sc.nextLine();
 
+                if (s2.contains("-")) {
+                    throw new ErroNumeroNegativo("Por favor, insira apenas valores positivos!");
+                }
+
                 String[] aux2 = s2.split(" ");
-                Double altura = Double.parseDouble(aux2[0]);
+                Double altura = Double.parseDouble(aux2[0].replace(",", "."));
 
                 Pessoa pessoa = new Pessoa(peso, altura);
                 System.out.printf("Seu imc é: %.2f", pessoa.calcularIMC());
                 loop = false;
             }
-            catch (InputMismatchException e) {
-                System.out.println("Por favor, insira um valor válido!");
-                sc.nextLine();
+            catch (ErroNumeroNegativo e) {
+                System.out.println(e.getMessage());
             }
             catch (NumberFormatException e) {
-                System.out.println("Por favor, utilize '.' ao invés de ','!");
+                System.out.println("Por favor, insira um valor válido!!");
             }
         }
         sc.close();
---
+++ entities/exceptions/ErroNumeroNegativo.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class ErroNumeroNegativo extends RuntimeException{
+    
+    public ErroNumeroNegativo(String msg) {
+        super(msg);
+    }
+}
