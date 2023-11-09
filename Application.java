c73c0e9ae5fc27d5e9b008aeda09e0eb96baab2b
GuiZimmermann <zimmerma83@gmail.com>
2023-11-09 14:19:51 -0400

    feat: tabela imc

--- Application.java
+++ Application.java
@@ -32,8 +32,32 @@ public class Application {
                 Double altura = Double.parseDouble(aux2[0].replace(",", "."));
 
                 Pessoa pessoa = new Pessoa(peso, altura);
-                System.out.printf("Seu imc é: %.2f", pessoa.calcularIMC());
+                Double imc = pessoa.calcularIMC();
+                System.out.printf("Seu imc é: %.2f%n", imc);
                 loop = false;
+                
+                if (imc < 17) {
+                    System.out.println("De acordo com o seu IMC, sua situação é: Muito abaixo do peso");
+                }
+                if (imc >= 17 && imc < 18.5) {
+                    System.out.println("De acordo com o seu IMC, sua situação é: Abaixo do peso");
+                }
+                if (imc >= 18.5 && imc < 25){
+                    System.out.println("De acordo com o seu IMC, sua situação é: Peso normal");
+                }
+                if (imc >= 25 && imc < 30) {
+                    System.out.println("De acordo com o seu IMC, sua situação é: Acima do peso");
+                }
+                if (imc >= 30 && imc < 35){
+                    System.out.println("De acordo com o seu IMC, sua situação é: Obesidade");
+                }
+                if (imc >= 35 && imc < 40) {
+                    System.out.println("De acordo com o seu IMC, sua situação é: Obesidade severa");
+                }
+                if (imc >= 40) {
+                    System.out.println("De acordo com o seu IMC, sua situação é: Obesidade mórbida");
+                }
+
             }
             catch (ErroNumeroNegativo e) {
                 System.out.println(e.getMessage());
